86ab5585d594c2d6e0f622cb0536e8ea477f6cbd
thedotwebstudio <dev.mukeshcgs@gmail.com>
2023-08-10 18:37:27 +0530

    start script added

--- app/controllers/tutorial.controller.js
+++ app/controllers/tutorial.controller.js
@@ -1,143 +1,149 @@
 const db = require("../models");
-const Tutorial = db.tutorials;
+const LinkedInUsers = db.linkedInUsers;
 
-// Create and Save a new Tutorial
+// Create and Save a new LinkedInUsers
 exports.create = (req, res) => {
   // Validate request
-  if (!req.body.title) {
+  if (!req.body.username) {
     res.status(400).send({ message: "Content can not be empty!" });
     return;
   }
 
-  // Create a Tutorial
-  const tutorial = new Tutorial({
-    title: req.body.title,
+  // Create a LinkedInUsers
+  const linkedInUsers = new LinkedInUsers({
+    username: req.body.username,
     description: req.body.description,
-    published: req.body.published ? req.body.published : false
+    published: req.body.published ? req.body.published : false,
   });
 
-  // Save Tutorial in the database
-  tutorial
-    .save(tutorial)
-    .then(data => {
+  // Save LinkedInUsers in the database
+  linkedInUsers
+    .save(linkedInUsers)
+    .then((data) => {
       res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while creating the Tutorial."
+          err.message ||
+          "Some error occurred while creating the LinkedInUsers.",
       });
     });
 };
 
 // Retrieve all Tutorials from the database.
 exports.findAll = (req, res) => {
-  const title = req.query.title;
-  var condition = title ? { title: { $regex: new RegExp(title), $options: "i" } } : {};
+  const username = req.query.username;
+  var condition = username
+    ? { username: { $regex: new RegExp(username), $options: "i" } }
+    : {};
 
-  Tutorial.find(condition)
-    .then(data => {
+  LinkedInUsers.find(condition)
+    .then((data) => {
       res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while retrieving tutorials."
+          err.message || "Some error occurred while retrieving linkedInUsers.",
       });
     });
 };
 
-// Find a single Tutorial with an id
+// Find a single LinkedInUsers with an id
 exports.findOne = (req, res) => {
   const id = req.params.id;
 
-  Tutorial.findById(id)
-    .then(data => {
+  LinkedInUsers.findById(id)
+    .then((data) => {
       if (!data)
-        res.status(404).send({ message: "Not found Tutorial with id " + id });
+        res
+          .status(404)
+          .send({ message: "Not found LinkedInUsers with id " + id });
       else res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res
         .status(500)
-        .send({ message: "Error retrieving Tutorial with id=" + id });
+        .send({ message: "Error retrieving LinkedInUsers with id=" + id });
     });
 };
 
-// Update a Tutorial by the id in the request
+// Update a LinkedInUsers by the id in the request
 exports.update = (req, res) => {
   if (!req.body) {
     return res.status(400).send({
-      message: "Data to update can not be empty!"
+      message: "Data to update can not be empty!",
     });
   }
 
   const id = req.params.id;
 
-  Tutorial.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
-    .then(data => {
+  LinkedInUsers.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then((data) => {
       if (!data) {
         res.status(404).send({
-          message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found!`
+          message: `Cannot update LinkedInUsers with id=${id}. Maybe LinkedInUsers was not found!`,
         });
-      } else res.send({ message: "Tutorial was updated successfully." });
+      } else res.send({ message: "LinkedInUsers was updated successfully." });
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message: "Error updating Tutorial with id=" + id
+        message: "Error updating LinkedInUsers with id=" + id,
       });
     });
 };
 
-// Delete a Tutorial with the specified id in the request
+// Delete a LinkedInUsers with the specified id in the request
 exports.delete = (req, res) => {
   const id = req.params.id;
 
-  Tutorial.findByIdAndRemove(id, { useFindAndModify: false })
-    .then(data => {
+  LinkedInUsers.findByIdAndRemove(id, { useFindAndModify: false })
+    .then((data) => {
       if (!data) {
         res.status(404).send({
-          message: `Cannot delete Tutorial with id=${id}. Maybe Tutorial was not found!`
+          message: `Cannot delete LinkedInUsers with id=${id}. Maybe LinkedInUsers was not found!`,
         });
       } else {
         res.send({
-          message: "Tutorial was deleted successfully!"
+          message: "LinkedInUsers was deleted successfully!",
         });
       }
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message: "Could not delete Tutorial with id=" + id
+        message: "Could not delete LinkedInUsers with id=" + id,
       });
     });
 };
 
 // Delete all Tutorials from the database.
 exports.deleteAll = (req, res) => {
-  Tutorial.deleteMany({})
-    .then(data => {
+  LinkedInUsers.deleteMany({})
+    .then((data) => {
       res.send({
-        message: `${data.deletedCount} Tutorials were deleted successfully!`
+        message: `${data.deletedCount} Tutorials were deleted successfully!`,
       });
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while removing all tutorials."
+          err.message ||
+          "Some error occurred while removing all linkedInUsers.",
       });
     });
 };
 
 // Find all published Tutorials
 exports.findAllPublished = (req, res) => {
-  Tutorial.find({ published: true })
-    .then(data => {
+  LinkedInUsers.find({ published: true })
+    .then((data) => {
       res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while retrieving tutorials."
+          err.message || "Some error occurred while retrieving linkedInUsers.",
       });
     });
 };
--- app/routes/turorial.routes.js
+++ app/routes/turorial.routes.js
@@ -1,28 +1,28 @@
-module.exports = app => {
-  const tutorials = require("../controllers/tutorial.controller.js");
+module.exports = (app) => {
+  const linkedInUsers = require("../controllers/tutorial.controller.js");
 
   var router = require("express").Router();
 
-  // Create a new Tutorial
-  router.post("/", tutorials.create);
+  // Create a new LinkedInUsers
+  router.post("/", linkedInUsers.create);
 
   // Retrieve all Tutorials
-  router.get("/", tutorials.findAll);
+  router.get("/", linkedInUsers.findAll);
 
   // Retrieve all published Tutorials
-  router.get("/published", tutorials.findAllPublished);
+  router.get("/published", linkedInUsers.findAllPublished);
 
-  // Retrieve a single Tutorial with id
-  router.get("/:id", tutorials.findOne);
+  // Retrieve a single LinkedInUsers with id
+  router.get("/:id", linkedInUsers.findOne);
 
-  // Update a Tutorial with id
-  router.put("/:id", tutorials.update);
+  // Update a LinkedInUsers with id
+  router.put("/:id", linkedInUsers.update);
 
-  // Delete a Tutorial with id
-  router.delete("/:id", tutorials.delete);
+  // Delete a LinkedInUsers with id
+  router.delete("/:id", linkedInUsers.delete);
 
-  // Create a new Tutorial
-  router.delete("/", tutorials.deleteAll);
+  // Create a new LinkedInUsers
+  router.delete("/", linkedInUsers.deleteAll);
 
-  app.use("/api/tutorials", router);
+  app.use("/api/linkedInUsers", router);
 };
